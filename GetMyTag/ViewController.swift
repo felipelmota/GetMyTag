7838c106ab64e2c402036be538c148a14c663292
Felipe Mota <felipelmota@gmail.com>
2017-03-12 17:49:40 -0300

    Using Imagga API and getting TAGs

--- GetMyTag/ViewController.swift
+++ GetMyTag/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
     
@@ -74,10 +75,121 @@ extension ViewController: UIImagePickerControllerDelegate {
         
         imageView.image = image
         
+        takePictureButton.isHidden = true
+        progressView.progress = 0.0
+        progressView.isHidden = false
+        activityIndicatorView.startAnimating()
+        
+        upload(
+            image: image,
+            progressCompletion: { [unowned self] percent in
+                self.progressView.setProgress(percent, animated: true)
+            },
+            completion: { [unowned self] tags, colors in
+                self.takePictureButton.isHidden = false
+                self.progressView.isHidden = true
+                self.activityIndicatorView.stopAnimating()
+                
+                self.tags = tags
+                self.colors = colors
+                
+                self.performSegue(withIdentifier: "ShowResults", sender: self)
+        })
+        
         dismiss(animated: true)
     }
 }
 
 // MARK: - UINavigationControllerDelegate
 extension ViewController: UINavigationControllerDelegate {
+    
 }
+
+// MARK: - Networking calls
+extension ViewController {
+    func upload(image: UIImage,
+                progressCompletion: @escaping (_ percent: Float) -> Void,
+                completion: @escaping (_ tags: [String], _ colors: [PhotoColor]) -> Void) {
+        guard let imageData = UIImageJPEGRepresentation(image, 0.5) else {
+            print("Could not get JPEG representation of UIImage")
+            return
+        }
+        
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(imageData,
+                                         withName: "imagefile",
+                                         fileName: "image.jpg",
+                                         mimeType: "image/jpeg")
+        },
+            to: "http://api.imagga.com/v1/content",
+            headers: ["Authorization": "Basic YWNjX2Q5MjgxNWVmZmRmNzMxOTo1NzY3YmMwNGYyZTM5ZjIwZWY1ZjI3NGZlNmVhOWQ4Nw=="],
+            encodingCompletion: { encodingResult in
+                
+                switch encodingResult {
+                case .success(let upload, _, _):
+                    upload.uploadProgress { progress in
+                        progressCompletion(Float(progress.fractionCompleted))
+                    }
+                    upload.validate()
+                    
+                    upload.responseJSON { response in
+                        guard response.result.isSuccess else {
+                            print("Error while uploading file: \(response.result.error)")
+                            completion([String](), [PhotoColor]())
+                            return
+                        }
+                        
+                        guard let responseJSON = response.result.value as? [String: Any],
+                            let uploadedFiles = responseJSON["uploaded"] as? [[String: Any]],
+                            let firstFile = uploadedFiles.first,
+                            let firstFileID = firstFile["id"] as? String else {
+                                print("Invalid information received from service")
+                                completion([String](), [PhotoColor]())
+                                return
+                        }
+                        
+                        print("Content uploaded with ID: \(firstFileID)")
+                        
+                        self.downloadTags(contentID: firstFileID) { tags in
+                            completion(tags, [PhotoColor]())
+                        }
+                    }
+                case .failure(let encodingError):
+                    print(encodingError)
+                }
+            }
+        )
+    }
+    
+    func downloadTags(contentID: String, completion: @escaping ([String]) -> Void) {
+        Alamofire.request(
+            "http://api.imagga.com/v1/tagging",
+            parameters: ["content": contentID],
+            headers: ["Authorization": "Basic YWNjX2Q5MjgxNWVmZmRmNzMxOTo1NzY3YmMwNGYyZTM5ZjIwZWY1ZjI3NGZlNmVhOWQ4Nw=="]
+            )
+            .responseJSON { response in
+                guard response.result.isSuccess else {
+                    print("Error while fetching tags: \(response.result.error)")
+                    completion([String]())
+                    return
+                }
+                
+                guard let responseJSON = response.result.value as? [String: Any],
+                    let results = responseJSON["results"] as? [[String: Any]],
+                    let firstObject = results.first,
+                    let tagsAndConfidences = firstObject["tags"] as? [[String: Any]] else {
+                        print("Invalid tag information received from the service")
+                        completion([String]())
+                        return
+                }
+                
+                let tags = tagsAndConfidences.flatMap({ dict in
+                    return dict["tag"] as? String
+                })
+                
+                completion(tags)
+        }
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+platform :ios, '10.0'
+
+inhibit_all_warnings!
+use_frameworks!
+
+target 'GetMyTag' do
+  use_frameworks!
+
+  pod 'Alamofire', '~> 4.2.0'
+
+  target 'GetMyTagTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'GetMyTagUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
